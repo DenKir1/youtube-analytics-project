cbeedc170aa5321c5f580292369e7b0d6da411c1
Denis Kirdyankin <kird.den@rambler.ru>
2023-09-12 15:33:48 +0300

    Homework-1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,19 @@
-
+#import os
+import json
+#import isodate
+from googleapiclient.discovery import build
+#api_key: str = os.getenv("API_KEY_YT") #не достает ключ win8
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        api_key = 'AIzaSyDHHhbdyDSpGlWAktWNEsdFBiwCFuBq_XQ'  # передаем явно
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel_id = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel_id, indent=2, ensure_ascii=False))
